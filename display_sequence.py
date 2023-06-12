0a04fdc5eacb66f69b07a969ae8c0b21402eede9
James Rogers <jroger87@vols.utk.edu>
2023-06-12 09:32:45 -0400

    added pause function, colorbar in scientific notation

--- display_sequence.py
+++ display_sequence.py
@@ -8,6 +8,7 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.ticker import ScalarFormatter
 
 def plot_data(filename):
 	I, sigI, N, dataHeader, L = mc.extractMcStasData(filename)
@@ -18,8 +19,7 @@ def plot_data(filename):
 		# Call the mcstas2TIFF function to generate .tif files
 		imN, imI, sigI = mc.mcstas2TIFF(filename, save=False)
 	
-		# Set axis limits
-		ax.set_xlim(extent[0], extent[1])
+		# Set axis limits ax.set_xlim(extent[0], extent[1])
 		ax.set_ylim(extent[2], extent[3])
 	
 		unit1 = re.findall(r"\[(.*?)\]", dataHeader['xlabel'])
@@ -30,7 +30,14 @@ def plot_data(filename):
 		dy = (extent[3] - extent[2]) / (np.array(imI).shape[0] - 1)
 	
 		plt.imshow(np.array(imI), extent=extent, cmap='plasma', vmin=overall_min, vmax=overall_max)
-		plt.colorbar().set_label('Intensity [n/s]/ '+"{:.2e}".format(dx*dy)+' ['+unit1[0]+'*'+unit2[0]+']')
+
+		cbar = plt.colorbar()
+		cbar.set_label('Intensity [n/s]/ '+"{:.2e}".format(dx*dy)+' ['+unit1[0]+'*'+unit2[0]+']')
+		formatter = ScalarFormatter(useMathText=True)
+		formatter.set_powerlimits((-2,2))
+		cbar.formatter = formatter
+		cbar.update_ticks()
+
 		plt.xlabel(dataHeader['xlabel'])
 		plt.ylabel(dataHeader['ylabel'])
 		plt.title(dataHeader['component']+"\npos="+dataHeader['position'], pad=10)
@@ -38,7 +45,7 @@ def plot_data(filename):
 	else:
 		print("Unknown Data Type.")
 
-def animate(frame):
+def update_image(frame):
 	# Clear the current plot
 	plt.clf()
 
@@ -48,9 +55,19 @@ def animate(frame):
 	# Plot the data from the current file
 	plot_data(filename)
 
-	# Add any additional styling, labels, titles, etc.
+	# Pause the animation if the space bar is pressed
+	if pause:
+		animation.event_source.stop()
 
-	return
+	#return
+
+# Function to handle key presses
+def on_key_press(event):
+	global pause
+	if event.key == ' ':
+		pause = not pause
+		if not pause:
+			animation.event_source.start()
 
 if __name__ == '__main__':
 	# Argument parsing
@@ -89,7 +106,13 @@ if __name__ == '__main__':
 	plot_data(filenames[0])
 
 	# Create the animation
-	animation = FuncAnimation(fig, animate, frames=len(filenames), interval=600, repeat=True)
+	animation = FuncAnimation(fig, update_image, frames=len(filenames), interval=600, repeat=True)
+
+	# Pause flag to control animation state
+	pause = False
+
+	# Connect the key press event handler
+	fig.canvas.mpl_connect('key_press_event', on_key_press)
 
 	# Display the animation
 	plt.show()
